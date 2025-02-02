e6d5527d5f6c47ac857823d5abb4f98841c3b6a3
Zosha Chung <czosha@gmail.com>
2025-02-02 18:44:52 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+// Task 1 Customer Discounts 
+let purchaseAmount = 500;
+let finalAmount = purchaseAmount;
+
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.4;
+}
+console.log("Customer Discount Applied"); 
+console.log("Final Amount with discount", finalAmount);
+
