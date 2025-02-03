ef26f6a516e6b5b7e457161b6704dafff98e67f8
Zosha Chung <czosha@gmail.com>
2025-02-02 19:08:17 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,7 +6,14 @@ if (purchaseAmount > 100) {
     finalAmount = purchaseAmount * 0.4;
 }
 console.log("Customer Discount Applied"); 
-console.log(\`Final amount after discount: $${finalAmount}\`);
+console.log(`Final amount after discount: $${finalAmount}`);
 
 
 // Task 2 Sales Report 
+let sales = [200, 150, 500, 300, 450];
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+console.log(`Total Sales: $${totalSales}`);
